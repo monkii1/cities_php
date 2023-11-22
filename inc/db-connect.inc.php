8267e9f75183673ef767c15130d9de3997ab8b69
monkii1 <102854745+monkii1@users.noreply.github.com>
2023-11-22 19:20:30 +0100

    create basic project framework

---
+++ inc/db-connect.inc.php
@@ -0,0 +1,14 @@
+<?php
+require __DIR__ . '../../../password/password.php';
+
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=php_cities', 'php_cities', $pwd, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+}
+catch(PDOException $e) {
+    echo 'Probleme mit der Datenbankverbindung...';
+    die();
+}
+
+var_dump($pdo);
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+  require __DIR__ . "/inc/all.php";
+  
+  
+  require __DIR__ . "/views/index.view.php";
